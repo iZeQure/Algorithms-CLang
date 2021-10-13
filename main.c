d84b9be9936014540cdc91aa18818f93c9419342
Tobias Rosenvinge <TobiasRosenvinge@hotmail.com>
2021-10-13 09:30:41 +0200

    Added QuickSort algorithm.

--- main.c
+++ main.c
@@ -3,7 +3,15 @@
 #include <time.h>
 
 #define ARRAY_LENGTH 20000
+#define LEN 5
 #define POPULATION_MAX_RANGE 1000000
+
+void run_me(void);
+void swap(int *x, int *y);
+void createArray(int *array, int length);
+void printArray(int *array, int length);
+void printArrayRange(int *begin, int *end);
+void quicksort(int *begin, int *end);
 
 void string_reverse(char *rawStr) {
     int len; char temp;
@@ -120,27 +128,115 @@ void populate_array(int *arr[], int n) {
 int main(void) {
     int numbers[ARRAY_LENGTH];
 
-    // Fill the array with random integers.
     populate_array((int **) numbers, ARRAY_LENGTH);
-//    printf("Array is filled with data.\n");
-
-//    printf("Starting benchmarks!\n");
     clock_t t = clock();
-//    printf("Clock started.\n");
 
-    // Sort array after the selection sort algorithm.
-//    selection_sort((int **) numbers, ARRAY_LENGTH)
     merge_sort_algorithm((int **) numbers, 0, ARRAY_LENGTH);
-//    printf("Array sorted with Selected Algorithm.\n");
 
     t = clock() - t;
     double time = ((double)t) / CLOCKS_PER_SEC;
     printf("Finished in %f seconds.\n", time);
 
-//    printf("Sorted: \n");
-//    for (int i = 0; i < ARRAY_LENGTH; ++i) {
-//        printf(" %-4d ", numbers[i]);
-//    }
+    clock_t t2 = clock();
+
+    run_me();
+
+    t = clock() - t;
+    double time2 = ((double)t) / CLOCKS_PER_SEC;
+    printf("Finished in %f seconds.\n", time2);
 
     return 0;
 }
+
+void run_me( void)
+{
+    int array[LEN];
+    createArray(&array, LEN);
+    printf("Array made\n");
+    printArray(&array, LEN);
+    int *begin, *end;
+    begin = &array;
+    end = begin + LEN - 1;
+    quicksort(begin, end);
+    printArray(array, LEN);
+}
+
+void quicksort(int *begin, int *end)
+{
+    printf("New call, array to sort:\n");
+    printArrayRange(begin, end);
+    if(begin >= end) return; //there is one or less to sort
+    int *p1, *p2;
+    p1 = begin;
+    p2 = end;
+    if((p2-p1)==1)
+    {
+        if(*p1>*p2)
+        {
+            swap(p1, p2);
+            printArrayRange(begin, end);
+        }
+        return;
+    }
+    int *pivot;
+    pivot = p1+((p2-p1)/2);
+    int middle = *pivot;
+    printf("middle: %d, starting sorting\n", *pivot);
+    while(p1<p2)
+    {
+        if(*p1 < middle) p1++;
+        else if(*p2 > middle) p2--;
+        else
+        {
+            swap(p1, p2);
+            p1++;
+            printArrayRange(begin, end);
+        }
+    }
+    if(*p1 >= middle) p1--;
+    printArrayRange(begin, end);
+    printf("done sorting\n");
+    quicksort(begin, p1);
+    p1++;
+    quicksort(p1, end);
+    return;
+}
+
+
+void swap(int *x, int *y) { // omskift int på plads x med int på plads y
+    int tmp;
+    tmp = *x;
+    *x = *y;
+    *y = tmp;
+}
+void createArray(int *array, int length)
+{
+    time_t t;
+    srand((unsigned) time(&t));
+    for(int i = 0 ; i < length ; i++ )
+    {
+        array[i] = rand() % 50;
+    }
+}
+void printArray(int *array, int length)
+{
+    int *ptr = array;
+    for(int i = 0 ; i < length ; i++ )
+    {
+        printf("%d ", *ptr);
+        ptr++;
+    }
+    printf("\n");
+}
+void printArrayRange(int *begin, int *end)
+{
+    int *ptr = begin;
+    while(ptr <= end)
+    {
+        printf("%d ", *ptr);
+        ptr++;
+    }
+    printf("\n");
+}
+
+
